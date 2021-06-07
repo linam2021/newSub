553d40d9d40548076c1168bc473c90da0b3802e9
englinab <lina.m.2021.m@gmail.com>
2021-06-07 12:48:36 +0300

    update of point condition

--- app/Http/Controllers/api/ChallengeController.php
+++ app/Http/Controllers/api/ChallengeController.php
@@ -215,7 +215,7 @@ class ChallengeController extends Controller
     public function getTrandingChallengesPagination()
     {
        try {
-            $challenges= Challenge::select(['challenges.user_id','challenges.hero_instagram','is_challengVerified', 'challenges.points as score'])
+            $challenges= Challenge::select(['challenges.user_id','challenges.hero_instagram','is_challengVerified', 'challenges.points as score', 'challenges.challengeDaysCount'])
             ->where('in_leader_board', 1)->orderByDesc('priority')->orderByDesc('points')->orderBy('created_at')->paginate(50);
 
             foreach($challenges as $challenge)
